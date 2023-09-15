51505bd4d1644eed60200432fcad6db763b6af42
quimeyxd6 <quimeyxd6@gmail.com>
2023-09-14 23:52:51 -0300

    inicia lado quipeke

--- JavaScript.js
+++ JavaScript.js
@@ -11,13 +11,22 @@ center.classList.add("outline", "center");
 const mainSection = document.createElement("div");
 mainSection.classList.add("outline", "mainSection");
 
-principal.appendChild(header);
-principal.appendChild(center);
-principal.appendChild(mainSection);
-
 const btn = document.createElement("button");
 const probando = document.createElement("span");
 probando.classList.add("button_top");
 probando.textContent = "boton";
 header.appendChild(btn);
 btn.appendChild(probando);
+
+//////////////////////////////////////////////
+
+const cuadroTexto = document.createElement("input");
+cuadroTexto.classList.add("outline", "cuadroTexto");
+center.appendChild(cuadroTexto);
+
+principal.appendChild(header);
+principal.appendChild(center);
+principal.appendChild(mainSection);
+
+
+
