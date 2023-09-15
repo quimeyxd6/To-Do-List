c59d0f71715c09220b43313a9bbf60bd833bf334
quimeyxd6 <quimeyxd6@gmail.com>
2023-09-14 22:54:20 -0300

    001

--- JavaScript.js
+++ JavaScript.js
@@ -2,3 +2,9 @@
 
 const principal = document.getElementById("main");
 
+const primerDiv = document.createElement("div");
+primerDiv.classList.add("probando");
+
+principal.appendChild(primerDiv);
+
+console.log("pito");
