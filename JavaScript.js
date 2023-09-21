3b6e89addcab6d74ad0fe68576bf59cf9dba1e24
quimeyxd6 <quimeyxd6@gmail.com>
2023-09-20 23:31:09 -0300

    restructuring

--- JavaScript.js
+++ JavaScript.js
@@ -1,61 +1,13 @@
 'use strict'
 
-const principal = document.getElementById("main");
+const mode = document.getElementById('btn_mode');
+const task = document.getElementById('input_task');
+const btnTask = document.getElementById('btn_task');
+const listTask = document.getElementById('ul_task');
+const listComplete = document.getElementById('ul_complete');
 
-const header = document.createElement("div");
-header.classList.add("outline", "header");
-
-const center = document.createElement("div");
-center.classList.add("outline", "center");
-
-const mainSection = document.createElement("div");
-mainSection.classList.add("outline", "mainSection");
-
-const btn = document.createElement("button");
-const probando = document.createElement("span");
-probando.classList.add("button_top");
-probando.textContent = "boton";
-header.appendChild(btn);
-btn.appendChild(probando);
-
-//////////////////////////////////////////////
-
-const cuadroTexto = document.createElement("input");
-cuadroTexto.classList.add("outline", "cuadroTexto");
-cuadroTexto.placeholder = "agregar tarea";
-center.appendChild(cuadroTexto);
-
-/////////////////////////////////////////////
-
-const list = document.createElement("div");
-list.classList.add("list_container");
-mainSection.appendChild(list);
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
 
 
-const list2 = document.createElement("div");
-list2.classList.add("list_container");
-mainSection.appendChild(list2);
-
-/////////////////////////////////////////////
-
-cuadroTexto.addEventListener('keypress', (e)=> {
-    if(e.key === 'Enter'){
-        tomarTarea();
-    }
 })
-
-function tomarTarea(){
-    const text = cuadroTexto.value;
-    const item = document.createElement("p");
-    
-    item.textContent = text;
-    list.appendChild(item);
-}
-
-
-principal.appendChild(header);
-principal.appendChild(center);
-principal.appendChild(mainSection);
-
-
-
