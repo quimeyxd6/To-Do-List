37b7477aa781956e3e229ac8e314d8912d851042
quimeyxd6 <quimeyxd6@gmail.com>
2023-09-25 15:12:40 -0300

    take and show array

--- JavaScript.js
+++ JavaScript.js
@@ -2,46 +2,38 @@
 
 const mode = document.getElementById('btn_mode');
 
-let task = '';
-const tasks = [];
+const tareas = [];
 
-function addTask(){
-    task = document.getElementById('input_task').value;
-    if (task !== '') {
-        tasks.push(task);
+const complete = [];
+
+
+function agregar() {
+    const tarea = document.getElementById('input_task').value;
+    if (tarea !== '') {
+        tareas.push(tarea);
         document.getElementById('input_task').value = '';
-        showTasks();
-    }
+        console.log(tareas);
+        mostrarTareas();
+    };
+};
+
+const listaTareas = document.getElementById('task_container');
+function mostrarTareas() {
+        const prueba = tareas.slice(-1);
+        const item = document.createElement('li');
+        item.textContent = prueba;
+        listaTareas.appendChild(item);
+        const btnComplete = document.createElement('button');
+        listaTareas.appendChild(btnComplete);
+        const btnDelete = document.createElement('button');
+        listaTareas.appendChild(btnDelete);
 }
 
-const taskList = document.getElementById('task_container');
-function showTasks() {
-    
-    taskList.innerHTML = ''; // Limpiar la lista antes de agregar las tareas nuevamente
-  
-    for (let i = 0; i < task.length; i++) {
-        const taskItem = document.createElement('div');
-        taskItem.classList.add('item_div');
-        const item = document.createElement('li');
-        item.classList.add('item');
-        taskItem.appendChild(item);
-        const btnAdd = document.createElement('button');
-        btnAdd.classList.add('btn_complete');
-        taskItem.appendChild(btnAdd);
-        const btnDelete = document.createElement('button');
-        btnDelete.classList.add('btn_delete');
-        taskItem.appendChild(btnDelete);
 
 
-        item.textContent = tasks[i];
-        taskList.appendChild(taskItem);
-    }
-  }
+document.getElementById('input_task').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter') {
+        agregar();
+    };
+});
 
-const prueba= document.getElementById('input_task').addEventListener('keydown', (e)=>{
-    if(e.key === 'Enter'){
-        addTask()
-    }
-})
-
-console.log(tasks);
