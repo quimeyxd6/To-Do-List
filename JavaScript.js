b85010630a00b73544f896f04e6698d8e8e5437f
quimeyxd6 <quimeyxd6@gmail.com>
2023-09-25 14:14:24 -0300

    array

--- JavaScript.js
+++ JavaScript.js
@@ -1,24 +1,47 @@
 'use strict'
 
 const mode = document.getElementById('btn_mode');
-const taskInput = document.getElementById('input_task');
 
-const taskList = [];
+let task = '';
+const tasks = [];
 
 function addTask(){
-    const task = taskInput.value;
+    task = document.getElementById('input_task').value;
     if (task !== '') {
-        taskList.push(task);
-
-        taskInput.value = '';
+        tasks.push(task);
+        document.getElementById('input_task').value = '';
+        showTasks();
     }
 }
 
+const taskList = document.getElementById('task_container');
+function showTasks() {
+    
+    taskList.innerHTML = ''; // Limpiar la lista antes de agregar las tareas nuevamente
+  
+    for (let i = 0; i < task.length; i++) {
+        const taskItem = document.createElement('div');
+        taskItem.classList.add('item_div');
+        const item = document.createElement('li');
+        item.classList.add('item');
+        taskItem.appendChild(item);
+        const btnAdd = document.createElement('button');
+        btnAdd.classList.add('btn_complete');
+        taskItem.appendChild(btnAdd);
+        const btnDelete = document.createElement('button');
+        btnDelete.classList.add('btn_delete');
+        taskItem.appendChild(btnDelete);
 
 
-const prueba = document.getElementById('btn_task');
-prueba.addEventListener('click', ()=>{
-    addTask();
+        item.textContent = tasks[i];
+        taskList.appendChild(taskItem);
+    }
+  }
+
+const prueba= document.getElementById('input_task').addEventListener('keydown', (e)=>{
+    if(e.key === 'Enter'){
+        addTask()
+    }
 })
 
-console.log(taskList)
+console.log(tasks);
