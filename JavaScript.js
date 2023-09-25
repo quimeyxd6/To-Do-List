cc0386d0bec77b5180e0c3f61e504b161db601aa
quimeyxd6 <quimeyxd6@gmail.com>
2023-09-25 15:48:20 -0300

    error when moving list item

--- JavaScript.js
+++ JavaScript.js
@@ -4,9 +4,6 @@ const mode = document.getElementById('btn_mode');
 
 const tareas = [];
 
-const complete = [];
-
-
 function agregar() {
     const tarea = document.getElementById('input_task').value;
     if (tarea !== '') {
@@ -15,25 +12,46 @@ function agregar() {
         console.log(tareas);
         mostrarTareas();
     };
-};
+};//Funciona
 
 const listaTareas = document.getElementById('task_container');
 function mostrarTareas() {
         const prueba = tareas.slice(-1);
+        const elemento = document.createElement('div');
+        elemento.setAttribute('id', 'elemento');
         const item = document.createElement('li');
         item.textContent = prueba;
-        listaTareas.appendChild(item);
+        elemento.appendChild(item);
         const btnComplete = document.createElement('button');
-        listaTareas.appendChild(btnComplete);
+        btnComplete.classList.add('boton');
+        btnComplete.setAttribute('id', 'complete');
+        elemento.appendChild(btnComplete);
         const btnDelete = document.createElement('button');
-        listaTareas.appendChild(btnDelete);
+        btnDelete.classList.add('boton');
+        elemento.appendChild(btnDelete);
+        listaTareas.appendChild(elemento);
+}//Funciona
+
+const listaCompleta = document.getElementById('task_complete');
+function tareasCompletas() {
+    const mover = document.getElementById('elemento');
+    if(mover){
+        listaTareas.removeChild(mover);
+        listaCompleta.appendChild(mover);
+    }
+    //Tomo elemento, pero no especifico cual, a corregir
 }
-
-
 
 document.getElementById('input_task').addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         agregar();
     };
 });
+
+const accion = document.getElementById('complete');
+if(accion){
+    accion.addEventListener('click', ()=>{
+        tareasCompletas();
+    })
+}
 
